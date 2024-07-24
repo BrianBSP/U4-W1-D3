7400c894e4513b9d3448316c277f385221532246
BrianBSP <167891999+BrianBSP@users.noreply.github.com>
2024-07-24 16:13:43 +0200

    svolto primo esercizio

--- src/es1/Main.java
+++ src/es1/Main.java
@@ -2,6 +2,17 @@ package es1;
 
 public class Main {
     public static void main(String[] args) {
-        
+
+        Rettangolo rettangolo1 = new Rettangolo(20, 30);
+        Rettangolo rettangolo2 = new Rettangolo(15, 45);
+        Rettangolo rettangolo3 = new Rettangolo();
+
+        rettangolo1.stampaRettangolo();
+        rettangolo2.stampaRettangolo();
+
+        rettangolo3.stampaDueRettangoli(rettangolo1, rettangolo2);
+
     }
+
+
 }
---
+++ src/es1/Rettangolo.java
@@ -0,0 +1,50 @@
+package es1;
+
+public class Rettangolo {
+    // Attributi
+    private double base;
+    private double altezza;
+
+    // Costruttori
+    public Rettangolo(double base, double altezza) {
+        this.base = base;
+        this.altezza = altezza;
+    }
+
+    public Rettangolo() {
+    }
+
+    // Metodi
+    // Calcola perimetro
+    public double getPerimetro() {
+        return (base + altezza) * 2;
+    }
+
+    // Calcola area
+    public double getArea() {
+        return base * altezza;
+    }
+
+    // 1° consegna
+    public void stampaRettangolo() {
+        System.out.println("Metodo stampaRettangolo: ");
+        System.out.println("Perimetro: " + getPerimetro());
+        System.out.println("Area: " + getArea());
+    }
+
+    // 2° consegna
+    public void stampaDueRettangoli(Rettangolo rettangolo1, Rettangolo rettangolo2) {
+
+        System.out.println("Rettangolo 1: ");
+        rettangolo1.stampaRettangolo();
+        System.out.println("rettangolo 2: ");
+        rettangolo2.stampaRettangolo();
+
+        double sommaPerimetri = rettangolo1.getPerimetro() + rettangolo2.getPerimetro();
+        double sommaAree = rettangolo1.getArea() + rettangolo2.getArea();
+
+        System.out.println("Somma dei perimetri: " + sommaPerimetri);
+        System.out.println("Somma delle aree: " + sommaAree);
+
+    }
+}
